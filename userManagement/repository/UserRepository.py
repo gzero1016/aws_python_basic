0f5b4c4e445bec6d0d44cb04bed0d6b71c3b6799
gzero1016 <gzero1016@gmail.com>
2023-09-12 17:48:55 +0900

    feat: db연결 및 insert

---
+++ userManagement/repository/UserRepository.py
@@ -0,0 +1,16 @@
+from userManagement.config.DataBaseConfig import DataBaseConfig, pymysql
+
+class UserRepository:
+
+    @staticmethod
+    def saveUser(user = None):
+        connection = DataBaseConfig.getConnection()
+        cursor = connection.cursor(pymysql.cursors.DictCursor)
+        sql = f"""
+insert into user_tb
+values(0, %s, %s, %s, %s)
+"""
+        # 알아서 %s 자리에 넣어줌
+        insertCount = cursor.execute(sql, (user.username, user.password, user.name, user.email))
+        connection.commit()
+        return insertCount
